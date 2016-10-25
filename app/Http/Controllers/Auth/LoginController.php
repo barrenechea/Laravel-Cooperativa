f3a8b6bf72a04328b0988d82f6d005f89f5e300f
Sebastián Barrenechea <sebastian@barrenechea.cl>
2016-10-25 03:25:34 -0300

    Test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -32,41 +32,6 @@ class LoginController extends Controller
      */
     protected $redirectTo = '/home';
 
-    public function attempt(array $credentials = [], $remember = false, $login = true)
-    {
-        $this->fireAttemptEvent($credentials, $remember, $login);
-
-        $this->lastAttempted = $user = $this->provider->retrieveByCredentials($credentials);
-
-        // If an implementation of UserInterface was returned, we'll ask the provider
-        // to validate the user against the given credentials, and if they are in
-        // fact valid we'll log the users into the application and return true.
-        if ($this->hasValidCredentials($user, $credentials)) {
-            if ($login) {
-                $validation = User::where('username', $credentials['username'])->first();
-                if($validation->is_admin)
-                {
-                    if($validation->roles->count() == 0)
-                    {
-                        return false;
-                    }
-                }
-                // Todo validacion a socios
-                $this->login($user, $remember);
-            }
-            return true;
-        }
-
-        // If the authentication attempt fails we will fire an event so that the user
-        // may be notified of any suspicious attempts to access their account from
-        // an unrecognized user. A developer may listen to this event as needed.
-        if ($login) {
-            $this->fireFailedEvent($user, $credentials);
-        }
-
-        return false;
-    }
-
     /**
      * Create a new controller instance.
      *
