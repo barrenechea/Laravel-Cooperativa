a405fd0ed1143360c770d0591d58bff7188af022
Sebastián Barrenechea <sebastian@barrenechea.cl>
2016-11-07 03:36:46 -0300

    New for Information tab

--- resources/views/system.blade.php
+++ resources/views/system.blade.php
@@ -3,7 +3,7 @@
 @section('htmlheader_title', 'Información del servidor')
 
 @section('contentheader_title')
-Servidor {{ php_uname() }}
+Ubuntu Server 16.04.1 LTS - Kernel  {{ str_replace('-generic', '', php_uname('r')) }}
 @endsection
 
 @section('main-content')
@@ -32,6 +32,44 @@ Servidor {{ php_uname() }}
         </div>
         <span class="progress-description">
           {{$pct}}% ocupado
+        </span>
+      </div>
+      <!-- /.info-box-content -->
+    </div>
+  </div>
+  <div class="col-sm-offset-3 col-md-offset-3 col-md-6 col-sm-6 col-xs-12">
+    <!-- small box -->
+    <div class="info-box bg-blue">
+      <span class="info-box-icon"><i class="fa fa-database"></i></span>
+
+      <div class="info-box-content">
+        <span class="info-box-text">Motor de base de datos</span>
+        <span class="info-box-number">{{ DB::connection()->getPdo()->query('select version()')->fetchColumn() }}</span>
+
+        <div class="progress">
+          <div class="progress-bar" style="width: 0"></div>
+        </div>
+        <span class="progress-description">
+          Versión del motor corriendo actualmente
+        </span>
+      </div>
+      <!-- /.info-box-content -->
+    </div>
+  </div>
+  <div class="col-sm-offset-3 col-md-offset-3 col-md-6 col-sm-6 col-xs-12">
+    <!-- small box -->
+    <div class="info-box bg-purple">
+      <span class="info-box-icon"><i class="fa fa-globe"></i></span>
+
+      <div class="info-box-content">
+        <span class="info-box-text">Servidor web</span>
+        <span class="info-box-number">{{ apache_get_version() }}</span>
+
+        <div class="progress">
+          <div class="progress-bar" style="width: 0"></div>
+        </div>
+        <span class="progress-description">
+          Versión del servidor web corriendo actualmente
         </span>
       </div>
       <!-- /.info-box-content -->
