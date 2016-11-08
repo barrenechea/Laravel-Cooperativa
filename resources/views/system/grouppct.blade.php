e6b231ae3d83f72af7cc67867b1e0b0c6af2756a
Sebastián Barrenechea <sebastian@barrenechea.cl>
2016-11-07 23:50:16 -0300

    Another fix

--- resources/views/system/grouppct.blade.php
+++ resources/views/system/grouppct.blade.php
@@ -9,7 +9,7 @@
   <div class="col-md-12">
    <div class="box box-primary">
     <div class="box-header with-border">
-      <h3 class="box-title">Asignar porcentajes - {{ Session::get('group')->description }}</h3>
+      <h3 class="box-title">Asignar porcentajes - {{ Cache::get('group')->description }}</h3>
     </div>
     <!-- /.box-header -->
     <!-- form start -->
