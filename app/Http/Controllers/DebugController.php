b0292a547a592a27e537cc7ed92c53b1af30a72f
Sebastián Barrenechea <sebastian@barrenechea.cl>
2017-04-23 15:22:46 -0300

    Testing dates

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -6,12 +6,18 @@ use Illuminate\Http\Request;
 use App\Bill;
 use App\Billdetail;
 use App\Payment;
+use Carbon\Carbon;
 
 class DebugController extends Controller
 {
     public function debug(Request $request)
     {
+    	$date = Carbon::createFromFormat('Y-m-d', '2016-12-01');
+    	$text = [];
+    	while ($date->day != 28 && $date->month != 2) {
+    		$text[] = $date->toDateTimeString();
+    	}
     	$bills = Bill::all();
-    	return json_encode($bills);
+    	return json_encode($text);
     }
 }
